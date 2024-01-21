1f620c4601064027666d2d58ecc3c8e6718c9162
inatonix <imitation0813@gmail.com>
2024-01-21 13:47:13 +0900

    commit for settings modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import TweetForm from "./components/TweetForm.vue";
 import TweetList from "./components/TweetList.vue";
+import SettingsModal from "./components/SettingsModal.vue";
 import { ref } from "vue";
 import { Tweet } from "./types/Tweet";
 
@@ -12,23 +13,43 @@ const tweets = ref<Tweet[]>([
 const onSubmitForm = (tweet: string) => {
   tweets.value.push({ id: String(Math.random()), text: tweet });
 };
+
+const isModalOpen = ref(false);
+const onClickSettings = () => {
+  isModalOpen.value = true;
+};
 </script>
 
 <template>
-  <div><TweetForm @submit="onSubmitForm" /> <TweetList :tweets="tweets" /></div>
+  <div class="container">
+    <div class="header">
+      <button @click="onClickSettings">Settings</button>
+    </div>
+    <Teleport to="body">
+      <SettingsModal v-if="isModalOpen" />
+    </Teleport>
+    <TweetForm @submit="onSubmitForm" />
+    <TweetList :tweets="tweets" />
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  display: flex;
+  flex-direction: column;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.header {
+  display: flex;
+  justify-content: flex-end;
+  padding: 1em;
+  button {
+    border-radius: 0.5em;
+    background-color: #81d6ee;
+    color: white;
+    width: 120px;
+    height: 50px;
+    font-size: 1em;
+  }
 }
 </style>
---
+++ src/components/SettingsModal.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="modal-mask">
+    <div class="modal-container">
+      <div class="modal-header">
+        <slot name="header">Settings</slot>
+      </div>
+
+      <div class="modal-body">
+        <slot name="body">
+          <p>Settings</p>
+        </slot>
+      </div>
+      <div class="modal-footer">
+        <slot name="footer">
+          <button>OK</button>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
